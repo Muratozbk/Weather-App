361b78fa5b1a520ab8355b593ba5147e55f64175
muratozbk <slipstream11@yandex.com>
2023-04-27 18:32:44 +0300

    weather appid

--- client/script.js
+++ client/script.js
@@ -2,17 +2,23 @@ import axios from "axios"
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
 
 function positionSuccess({ coords }) {
-    getWeather(coords.latiude, coords.longitude)
+    getWeather(coords.latitude, coords.longitude)
 }
 
 function positionError() {
     alert('There was an error getting your location. Please allow us to use your location and refresh the page')
 }
-console.log(12222)
 
 function getWeather(lat, lon) {
-    axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${'19b6050a15da8e692b5028e925466c6c'}`).
-        then(res => {
+    axios
+        .get("http://localhost:3001/weather", {
+            params: { lat, lon },
+        })
+        .then(res => {
             console.log(res.data)
+        })
+        .catch(e => {
+            console.log(e)
+            // alert("Error getting weather. Please try again.")
         })
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
 const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+// const weatherData = require('./example.json')
 
-app.get('/weather', (req, res) => {
-
+app.get("/weather", (req, res) => {
+    const { lat, lon } = req.query
+    console.log(lat, lon)
+    axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            lat,
+            lon,
+            appid: '7eeba2fe33e21f0febbd0f44c3522f47',
+            // unit: 'imperial',
+            // exclude: "minutely,alerts",
+        }
+    }).then(({ data }) => {
+        res.json(data)
+    }).catch(e => {
+        console.log(e)
+        res.sendStatus(500)
+    })
 })
 
 app.listen(3001)
+
